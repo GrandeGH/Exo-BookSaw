4b19f817e53b4facb988d8a8efd03ce436ed2ba1
GrandeGH <georgesgrande96@gmail.com>
2025-01-24 16:41:47 +0100

    2e commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let btnDarkMode = document.querySelector(".btnDarkMode")
+let section = document.querySelector("section")
+// let p = document.querySelectorAll("p")
+
+btnDarkMode.addEventListener("click", function () {
+    section.classList.toggle("darkModeSection") 
+
+})
